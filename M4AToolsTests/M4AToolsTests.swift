5ed51b70e1a0275664bd0cec35346a5260b22dfd
Andrew Hyatt <ahyattdev@icloud.com>
2018-02-08 11:27:24 -0500

    Initial Commit

---
+++ M4AToolsTests/M4AToolsTests.swift
@@ -0,0 +1,36 @@
+//
+//  M4AToolsTests.swift
+//  M4AToolsTests
+//
+//  Created by Andrew Hyatt on 2/8/18.
+//  Copyright © 2018 Andrew Hyatt. All rights reserved.
+//
+
+import XCTest
+@testable import M4ATools
+
+class M4AToolsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
